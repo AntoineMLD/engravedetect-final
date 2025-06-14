4e21384786d0ba69ebfe9048f5deef61ccd155d3
AntoineMLD <antoine.moulard@hotmail.com>
2025-06-15 00:26:06 +0200

    fix cicd error

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from engravedetect.api.routes.v1 import auth, verres
-from engravedetect.api.core.config import settings
+from src.api.routes.v1 import auth, verres
+from src.api.core.config import settings
 
 # Création de l'application FastAPI
 app = FastAPI(
